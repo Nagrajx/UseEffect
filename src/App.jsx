df0b33b8ea664aadc4fda25818eb11e2c57a6ab5
Nagrajx <satishnagraj54@gmail.com>
2026-06-10 16:37:39 +0530

    Add type of pagination

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ const App = () => {
 
   const [userdata, setUserData] = useState([])
 
+  const [index ,setIndex] = useState(1);
+
   const getData = async () => {
-    const data = await axios.get('https://picsum.photos/v2/list?page=2 ')
+    const data = await axios.get(`https://picsum.photos/v2/list?page=${index} `)
     setUserData(data.data)
   }
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [index]);
+
+  const moreData = ()=>{
+     
+  }
 
 
 
@@ -34,12 +40,30 @@ const App = () => {
 
   return (
     <div className='bg-black h-screen p-4 overflow-auto text-white'>
-      
+          <h1 className='w-[100px] h-[50px]  flex items-center justify-center rounded text-4xl bg-red-400'>{index}</h1>
       <div className='flex flex-wrap justify-between mt-10 gap-2 space-y-3'>
         {
           printUserData
         }
       </div>
+     
+      <div className='flex justify-center mt-40 gap-5 p-10 '>
+        <button  onClick={()=>{
+           if(index>1){
+             setIndex(index-1)
+           }
+        }} className='bg-amber-400 p-3 rounded  text-lg font-bold active:scale-90'>
+        Prev
+      </button>
+
+      <button 
+       onClick={()=>{
+         setIndex(index+1);
+       }}
+       className='bg-amber-400 p-3 rounded text-lg font-bold active:scale-90 '>Next</button>
+      </div>
+
+
     </div>
   )
 }
