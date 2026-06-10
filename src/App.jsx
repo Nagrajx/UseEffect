b339ffb4835bc61b2d8a5568d09e73b44d3be37f
Nagrajx <satishnagraj54@gmail.com>
2026-06-10 22:36:16 +0530

    Adding try and catch

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,39 @@ const App = () => {
 
   const [userdata, setUserData] = useState([])
 
-  const [index ,setIndex] = useState(1);
+  const [index, setIndex] = useState(1);
+
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
-    const data = await axios.get(`https://picsum.photos/v2/list?page=${index} `)
-    setUserData(data.data)
+    try {
+      setLoading(true)
+      const data = await axios.get(`https://picsum.photos/v2/list?page=${index} `)
+      setUserData(data.data)
+    }
+    catch (error) {
+      console.log(error);
+    }
+    finally {
+      setLoading(false)
+    }
+
+
   }
 
   useEffect(() => {
     getData();
   }, [index]);
 
-  const moreData = ()=>{
-     
-  }
 
 
 
-  let printUserData = <h1 className='text-bold'>No Data Available</h1>
+
+  let printUserData = <h1 className='text-bold'>Loading...</h1>
 
   if (userdata.length > 0) {
     printUserData = userdata.map(function (elem, idx) {
-      return <a href={elem.url}target='_blank'>
+      return <a href={elem.url} target='_blank'>
         <div key={idx} className='h-50 w-60  '  >
           <div>
             <img className='h-full object-cover rounded' src={elem.download_url} />
@@ -40,27 +51,31 @@ const App = () => {
 
   return (
     <div className='bg-black h-screen p-4 overflow-auto text-white'>
-          <h1 className='w-[100px] h-[50px]  flex items-center justify-center rounded text-4xl bg-red-400'>{index}</h1>
-      <div className='flex flex-wrap justify-between mt-10 gap-2 space-y-3'>
-        {
-          printUserData
-        }
-      </div>
-     
-      <div className='flex justify-center mt-40 gap-5 p-10 '>
-        <button  onClick={()=>{
-           if(index>1){
-             setIndex(index-1)
-           }
-        }} className='bg-amber-400 p-3 rounded  text-lg font-bold active:scale-90'>
-        Prev
-      </button>
+      <h1 className='w-[100px] h-[50px]  flex items-center justify-center rounded text-4xl bg-red-400'>{index}</h1>
+      {
+        loading ? (<h1 className='text-3xl font-bold text-center  mt-20'>
+          Loading...
+        </h1>) : (<div className='flex flex-wrap justify-between mt-10 gap-2 space-y-3 '>
+          {
+            printUserData
+          }
+        </div>)
+      }
 
-      <button 
-       onClick={()=>{
-         setIndex(index+1);
-       }}
-       className='bg-amber-400 p-3 rounded text-lg font-bold active:scale-90 '>Next</button>
+      <div className='flex justify-center mt-5 gap-5 p-10 '>
+        <button onClick={() => {
+          if (index > 1) {
+            setIndex(index - 1)
+          }
+        }} className='bg-amber-400 p-3 rounded  text-lg font-bold active:scale-90'>
+          Prev
+        </button>
+        <h1 className='mt-4'>Page {index}</h1>
+        <button
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+          className='bg-amber-400 p-3 rounded text-lg font-bold active:scale-90 '>Next</button>
       </div>
 
 
